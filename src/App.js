7fa35021f45e0d98f66948c11598766f6de3ea48
marcinzygan <soul@o2.pl>
2022-06-13 21:00:30 +0100

    added fuctionality to add new post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import PostList from "./features/PostList";
-
+import AddPostForm from "./features/post/AddPostForm";
 export default function App() {
   return (
     <main>
       <PostList />
+      <AddPostForm />
     </main>
   );
 }
--- src/features/post/AddPostForm.js
+++ src/features/post/AddPostForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-
+import { useDispatch } from "react-redux";
+import { postAdded } from "./postSlice";
+import { nanoid } from "@reduxjs/toolkit";
 import React from "react";
 
 const AddPostForm = () => {
@@ -9,13 +11,26 @@ const AddPostForm = () => {
   const onTitleChange = (e) => setTitle(e.target.value);
   const onContentChange = (e) => setContent(e.target.value);
 
+  const dispatch = useDispatch();
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        })
+      );
+      setTitle("");
+      setContent("");
+    }
+  };
+
   return (
     <section>
       <h2>Add a New Post</h2>
       <form>
-        <label htmlFor="" postTitle>
-          Post Title:
-        </label>
+        <label htmlFor="postTitle">Post Title:</label>
         <input
           type="text"
           id="postTitle"
@@ -23,16 +38,16 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChange}
         />
-        <label htmlFor="" postContent>
-          Content:
-        </label>
+        <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
           name="postContent"
           value={content}
           onChange={onContentChange}
         />
-        <button type="button">Save Post</button>
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
       </form>
     </section>
   );
