e16f15c4c70c88bf4fc9251664fdc005c8bb0268
marcinzygan <soul@o2.pl>
2022-06-21 19:45:19 +0100

    added route to display posts for each user

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import PostList from "./features/post/PostList";
 import AddPostForm from "./features/post/AddPostForm";
 import SinglePostPage from "./features/post/SinglePostPage";
 import EditPostForm from "./features/post/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 import Layout from "./components/Layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 export default function App() {
   return (
     <Routes>
@@ -15,6 +18,13 @@ export default function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
